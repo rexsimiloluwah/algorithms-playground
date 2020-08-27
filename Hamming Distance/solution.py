a6434a34d9688548289a8435b1d8488734a115c5
rexsimiloluwah <adetoyosi2001@gmail.com>
2020-08-27 14:10:50 +0100

    Hamming Distance

---
+++ Hamming Distance/solution.py
@@ -0,0 +1,22 @@
+# Simple implementation of Hamming distance 
+
+class HammingDistance:
+    @staticmethod
+    def compute(x, y):
+        """
+            Args:-
+            ------------------------------------------------------
+            x (int), y(int)
+            Returns :- 
+            ------------------------------------------------------
+            int :- Computed Hamming distance
+        """
+
+        # Simple solution (Compute the binary equivalent of the XOR of the two integers)
+        xor = bin(x ^ y).split('b')[-1]
+        return xor.count('1')
+
+if __name__ == "__main__":
+    h = HammingDistance()
+    print(h.compute(12,15))
+
