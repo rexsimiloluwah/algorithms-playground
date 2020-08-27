806032777fbc355d108872cef0d0a70277a15b49
rexsimiloluwah <adetoyosi2001@gmail.com>
2020-08-27 04:15:33 +0100

    Amicable Pairs

---
+++ Amicable Pairs/README.md
@@ -0,0 +1,15 @@
+PROBLEM DESCRIPTION :- 
+
+Check if two numbers are amicable or not
+
+Amicable numbers are two different numbers so related that the sum of the proper divisors of each is equal to the other number. (A proper divisor of a number is a positive factor of that number other than the number itself. For example, the proper divisors of 6 are 1, 2, and 3.)
+
+The smallest pair of amicable numbers is (220, 284). They are amicable because the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110, of which the sum is 284; and the proper divisors of 284 are 1, 2, 4, 71 and 142, of which the sum is 220.
+
+SAMPLE INPUT :- 
+a = 220
+b = 284 
+
+OUTPUT :- 
+
+Boolean => True (Because they are Amicable !)
---
+++ Amicable Pairs/solution.py
@@ -0,0 +1,30 @@
+# SOLUTION FOR AMICABLE PAIRS PROBLEM 
+from functools import reduce
+class Solution:
+
+    def find_factors(self, N):
+        """
+            Desc. : Basic algorithm to find the sum of all proper divisors of a number
+            Args (int) : N
+            Returns (int) : sum of the list of factors
+        """
+        factors = set(reduce(list.__add__, [[i, N//i] for i in range(1, int(N**0.5)+ 1) if N % i == 0]))
+        factors.remove(N)
+        return sum(list(factors))
+
+    def is_amicable(self, a,b):
+        """
+            Desc. : Checks for *amicability based on the given condition
+            Args (int,int): a,b
+            Returns (Boolean)
+        """
+        if (self.find_factors(a) == b) and (self.find_factors(b) == a):
+            return True
+        return False
+
+if __name__ == "__main__":
+    solution = Solution()
+    a = int(input())
+    b = int(input())
+
+    print(solution.is_amicable(a,b))
