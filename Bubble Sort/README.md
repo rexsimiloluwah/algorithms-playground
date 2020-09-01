f40bd2df9fe681fc1d0b1fd4a9f463141f6e37f4
rexsimiloluwah <adetoyosi2001@gmail.com>
2020-09-01 23:14:22 +0100

    New

---
+++ Bubble Sort/README.md
@@ -0,0 +1,3 @@
+Implementation of the Bubble Sort algorithm in python 
+
+TIME COMPLEXITY :- O(n**2)
---
+++ Counting Digits/solution.py
@@ -0,0 +1,15 @@
+
+def countingdigits(s):
+    
+    d = dict(enumerate([0]*10))
+    for k,v in d.items():
+        if str(k) in s:
+            h = s.count(str(k))
+            d[k]+=h
+            
+    for k,v in d.items():
+        print(k,v)
+
+if __name__ == "__main__":
+    s = str(input())
+    countingdigits(s)
